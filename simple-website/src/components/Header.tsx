8409cf1365477950033dbf6016475f3b04b9cd94
roon-replica <think-roon@naver.com>
2023-05-27 13:22:51 +0900

    [공통] 헤더 적용

---
+++ simple-website/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+interface Props {
+  header: string;
+}
+
+export default function Header({ header }: Props) {
+  return (
+    <>
+      <Head>
+        <title>This is head of your page</title>
+        <link rel="icon" href="/images/meow-popcorn.gif" />
+      </Head>
+      <header className="ml-6 max-w-screen-xl bg-gray body-font border-b border-gray-200">
+        <div className="container flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div className="flex title-font font-medium items-center text-gray-800 dark:text-gray-100 mb-4 md:mb-0">
+            <span className="ml-3 text-xl">{header}</span>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
